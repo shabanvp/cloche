7f4d5d008df61f507d8af241c1a4270d1226de51
muhammed shaban <shabanmuhammed426@gmail.com>
2026-02-27 08:03:51 +0530

    Integrate Supabase and add test-db endpoint

--- cloche-backend/db.js
+++ cloche-backend/db.js
@@ -1,10 +1,1 @@
-const mysql = require("mysql2");
-
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",          // change if needed
-  password: "shabansabu",          // your MySQL password
-  database: "cloche"
-});
-
-module.exports = db;
+module.exports = null;
